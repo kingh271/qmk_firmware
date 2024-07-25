07a6583f476e83cb15f7b80247491b6975ce3d99
kingh271 <60322034+kingh271@users.noreply.github.com>
2024-07-25 14:40:28 -0400

    Remove user files from master branch

--- users/kingh271/kingh271.c
+++
@@ -1,1 +0,0 @@
-#include "kingh271.h"
--- users/kingh271/kingh271.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include QMK_KEYBOARD_H
-
-
-#include "version.h"
-#ifdef TAP_DANCE_ENABLE
-#    include "tap_dances.h"
-#endif
-#include "keycodes.h"
